3845af29eb5e839a506eafb457ed9f294894ec6a
Sanskriti <sanskritisri27@gmail.com>
2025-03-01 14:29:20 +0530

    added navbar and routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
+import { Flex, Link, Box } from '@chakra-ui/react'
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Flex justify={'space-between'} padding={3}>
+      <Box>My Library</Box>
+      <Flex gap={4}>
+        <Link as={RouterLink} to='/'>
+          Home
+        </Link>
+        <Link as={RouterLink} to='/login'>
+          Login
+        </Link>
+        <Link as={RouterLink} to='/register'>
+          Register
+        </Link>
+        <Link as={RouterLink} to='/mybooks'>
+          My Books
+        </Link>
+      </Flex>
+    </Flex>
+    
   )
 }
 
