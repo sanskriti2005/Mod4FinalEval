e208c940ee37fdb242017f6c12a6521bc605f1a1
Sanskriti <sanskritisri27@gmail.com>
2025-03-01 15:40:47 +0530

    backend logic for user selected books to persist on login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Home = () => {
+  const { books } = useSelector(state => state.books);
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch
+  }, [])
   return (
     <div>Home</div>
   )
